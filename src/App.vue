eabaa772d88ad4215a0b887c12cfe88d7e1b5bf6
Coder-Delta <ranjitbhandary15@gmail.com>
2026-01-03 12:19:43 +0530

    feat: initialize Vue project with Vite and create basic components
    - Add package.json for project configuration and dependencies
    - Include favicon.ico for branding
    - Create App.vue as the main application component
    - Add global styles in main.css
    - Implement Footer.vue with creator information
    - Create Header.vue for the title of the application
    - Develop Main.vue for displaying quotes and interaction
    - Set up main.js to bootstrap the Vue application
    - Configure Vite with vue and vue-devtools plugins

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Header from '@/components/Header.vue';
+    import Footer from '@/components/Footer.vue';
+    import Main from '@/components/Main.vue';
+</script>
+
+<template>
+    <Header />
+    <Footer />
+    <Main />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        Created by <span>Professor Delta</span> @ 2026
+    </footer>
+</template>
+
+<style scoped>
+/* FOOTER CSS */
+footer {
+    position: fixed;
+    margin: 0 auto;
+    bottom: 0;
+    text-align: center;
+    color: #CCD6D9;
+    padding-bottom: 20px;
+}
+
+span {
+    font-family: cursive;
+}
+</style>
